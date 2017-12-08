f919a1bee1d6b60b4417182a71587d058b91ec93
ContractNet <contractnetlabs@gmail.com>
2017-12-08 07:39:58 -0800

    Homestead Block Reset

--- params/util.go
+++ params/util.go
@@ -27,10 +27,10 @@ var (
 	MainNetGenesisHash = common.HexToHash("0x5e12c356e2784188ad26587c739e7b69197f1bde246256188717f60e0b5acc06") // Mainnet genesis hash to enforce below configs on
 
 	TestNetHomesteadBlock = big.NewInt(0) // Testnet homestead block
-	MainNetHomesteadBlock = big.NewInt(100) // Mainnet homestead block
+	MainNetHomesteadBlock = big.NewInt(0) // Mainnet homestead block
 
 	TestNetHomesteadGasRepriceBlock = big.NewInt(0) // Testnet gas reprice block
-	MainNetHomesteadGasRepriceBlock = big.NewInt(110) // Mainnet gas reprice block
+	MainNetHomesteadGasRepriceBlock = big.NewInt(0) // Mainnet gas reprice block
 
 	TestNetHomesteadGasRepriceHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d") // Testnet gas reprice block hash (used by fast sync)
 	MainNetHomesteadGasRepriceHash = common.HexToHash("0x5e12c356e2784188ad26587c739e7b69197f1bde246256188717f60e0b5acc06") // Mainnet gas reprice block hash (used by fast sync)
